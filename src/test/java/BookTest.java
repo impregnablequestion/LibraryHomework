8cd54f0f011616030358331629864898cb2e1119
Séamus <ryans40@tcd.ie>
2023-02-07 19:53:03 +0000

    Made Book class

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+    Book book;
+    @Before
+    public void setUp () {
+        book = new Book("Lote", "Shola von Reinhold", "Fantasy");
+    }
+    @Test
+    public void canGetTitle () {
+        assertEquals("Lote", book.getTitle());
+    }
+
 }
