c35ca77cbdc38779b151f3bbfdef6aab8dc2cec0
Séamus <ryans40@tcd.ie>
2023-02-07 20:10:44 +0000

    Library MVP tests passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(Integer capacity) {
+        this.collection = new ArrayList<>();
+        this.capacity = capacity;
+    }
+    public int getTotalBooks(){
+        return this.collection.size();
+    }
+    public void addBook (Book book) {
+        int stock = getTotalBooks();
+        if (stock < this.capacity) {
+            this.collection.add(book);
+        }
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    Library pollokshieldsLib;
+    Book lote;
+
+    @Before
+    public void setUp () {
+        pollokshieldsLib = new Library(20);
+        lote = new Book("Lote","Shola von Reinhold", "Fantasy");
+
+    }
+    @Test
+    public void canGetTotalBooks () {
+        assertEquals(0, pollokshieldsLib.getTotalBooks());
+    }
+    @Test
+    public void canAddBook () {
+        pollokshieldsLib.addBook(lote);
+        assertEquals(1, pollokshieldsLib.getTotalBooks());
+    }
+    @Test
+    public void cantAddBookIfStockFull () {
+        Library tinyLib = new Library(2);
+        Book paul = new Book("Paul", "Daisy Lafarge", "Fiction");
+        Book normalppl = new Book("Normal People", "Sally Rooney", "Fiction");
+        tinyLib.addBook(lote);
+        tinyLib.addBook(paul);
+        tinyLib.addBook(normalppl);
+        assertEquals(2, tinyLib.getTotalBooks());
+    }
 }
